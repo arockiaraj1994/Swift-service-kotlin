8b886fda5b805b4eb3dafa29587ac0bce1989042
Arockiaraj Rayappan <arockiaraj@ubixi.com>
2016-12-19 20:54:06 +0530

    method sepeated while auto generate

--- src/main/java/com/example/template/MT202G.java
+++ src/main/java/com/example/template/MT202G.java
@@ -3,7 +3,12 @@ package com.example.template;
 
 import com.example.model.Transaction;
 import com.prowidesoftware.swift.model.SwiftBlock4;
+import com.prowidesoftware.swift.model.field.Field;
 import com.prowidesoftware.swift.model.field.Field32A;
+import com.prowidesoftware.swift.model.field.Field53B;
+import com.prowidesoftware.swift.model.field.Field57A;
+import com.prowidesoftware.swift.model.field.Field58A;
+import com.prowidesoftware.swift.model.mt.AbstractMT;
 
 
 /**
@@ -20,14 +25,61 @@ public class MT202G {
     public String getSwiftMessage(Transaction transaction) {
         // construct block 4
         SwiftBlock4 block4 = new SwiftBlock4();
+        // Add Field32A
+        block4 .append(getField32A(transaction));
+        // Add Field58A
+        block4 .append(getField58A(transaction));
+        // Add Field53B
+        block4 .append(getField53B(transaction));
+        // Add Field57A
+        block4 .append(getField57A(transaction));
+        AbstractMT abstractMT = AbstractMT.create(Integer.valueOf("202"));
+        abstractMT.getSwiftMessage().setBlock4(block4);
+        return abstractMT.message();
+    }
+
+    /**
+     * This auto generate field methods
+     * 
+     */
+    private Field getField32A(Transaction transaction) {
         Field32A field32A = new Field32A();
-        field32A.setCurrency(transaction.getCurrencyCode());
-        field32A.setAmount(transaction.getAmount());
-        field32A.setDate(transaction.getDate());
-        block4 .append(field32A);
-        MT202G myMT = new MT202G();
-        myMT.getSwiftMessage().setBlock4(block4);
-        return myMT.message();
+        field32A.setCurrency(transaction.CurrencyCode());
+        field32A.setAmount(transaction.Amount());
+        field32A.setDate(transaction.Date());
+        return field32A;
+    }
+
+    /**
+     * This auto generate field methods
+     * 
+     */
+    private Field getField58A(Transaction transaction) {
+        Field58A field58A = new Field58A();
+        field58A.setAccount(transaction.ReceiverAccountNumber());
+        field58A.setBIC(transaction.ReceiverInstitutionBIC());
+        return field58A;
+    }
+
+    /**
+     * This auto generate field methods
+     * 
+     */
+    private Field getField53B(Transaction transaction) {
+        Field53B field53B = new Field53B();
+        field53B.setAccount(transaction.SenderAccountNumber());
+        field53B.setLocation(transaction.SenderInstitutionName());
+        return field53B;
+    }
+
+    /**
+     * This auto generate field methods
+     * 
+     */
+    private Field getField57A(Transaction transaction) {
+        Field57A field57A = new Field57A();
+        field57A.setBIC(transaction.ReceiverBankBICReceiverBankBranch());
+        return field57A;
     }
 
 }
--- src/main/kotlin/com/example/build/Template.kt
+++ src/main/kotlin/com/example/build/Template.kt
@@ -13,47 +13,60 @@ import com.prowidesoftware.swift.model.SwiftBlock2
 import com.prowidesoftware.swift.model.SwiftBlock3
 import com.prowidesoftware.swift.model.SwiftBlock4
 import com.beust.klaxon.JsonArray
+import com.prowidesoftware.swift.model.mt.AbstractMT
+import com.prowidesoftware.swift.model.field.Field
 
 class Template {
     fun build() {
-        val templateObject = parse("src/main/template/MT202.json") as JsonObject
-        val fileName: String = "MT202G"
-        val codeModel = JCodeModel();
-        
-        
+        var templateObject = parse("src/main/template/MT202.json") as JsonObject
+        var fileName: String = "MT202G"
+        var codeModel = JCodeModel();
+
+
         // create package
-        val javaPackage = codeModel._package("com.example.template");
-        
+        var javaPackage = codeModel._package("com.example.template");
+
         var definedClass = javaPackage._class(fileName);
         definedClass.javadoc().add("Auto Generated class.");
         var method = definedClass.method(JMod.PUBLIC, String::class.java, "getSwiftMessage");
         method.javadoc().add("This method returns swift message");
         method.param(Transaction::class.java, "transaction");
         var block = method.body();
-        
+
         var methodParams = method.listParams();
         var swiftBlock4 = codeModel.ref(SwiftBlock4::class.java);
 
-                
+
         block.directStatement("// construct block 4");
         var block4Var = block.decl(swiftBlock4, "block4", JExpr._new(swiftBlock4));
-        
-        for ((key,value) in templateObject.get("block4") as JsonObject) {
-            var field = codeModel.ref(Class.forName("com.prowidesoftware.swift.model.field."+key));
-            var fieldVar = block.decl(field, (value as JsonObject).get("name") as String, JExpr._new(field));
-            
-            for(i in value.get("params") as JsonArray<JsonObject>){
-                for ((paramKey,paramValue) in i ) {
-                    println("set"+paramKey)
-                	block.add(fieldVar.invoke("set"+paramKey).arg(methodParams[0].invoke("get"+paramValue)))
+
+        for ((key, value) in templateObject.get("block4") as JsonObject) {
+
+            var fieldMethod = definedClass.method(JMod.PRIVATE, Field::class.java, "get" + key);
+            fieldMethod.javadoc().add("This auto generate field methods");
+            fieldMethod.param(Transaction::class.java, "transaction");
+            var methodBlock = fieldMethod.body();
+
+            var fieldMethodParams = fieldMethod.listParams();
+            var field = codeModel.ref(Class.forName("com.prowidesoftware.swift.model.field." + key));
+            var fieldVar = methodBlock.decl(field, (value as JsonObject).get("name") as String, JExpr._new(field));
+
+            for (i in value.get("params") as JsonArray<JsonObject>) {
+
+                for ((paramKey, paramValue) in i) {
+                    println("set" + paramKey)
+                    methodBlock.add(fieldVar.invoke("set" + paramKey).arg(fieldMethodParams[0].invoke((paramValue as String))))
                 }
-                block.add(block4Var.invoke("append").arg(fieldVar));
+                methodBlock._return(fieldVar);
             }
-            
+            block.directStatement("// Add "+key);
+            block.add(block4Var.invoke("append").arg(JExpr.invoke(fieldMethod).arg(methodParams[0])));
         }
-        var jVarMT = block.decl(definedClass,"myMT", JExpr._new(definedClass));
-        
-        if(block4Var != null){
+        var mtMessage = codeModel.ref(AbstractMT::class.java)
+        var jVarMT = block.decl(mtMessage, "abstractMT",
+                mtMessage.staticInvoke("create").arg(codeModel.ref(Integer::class.java).staticInvoke("valueOf").arg("202")));
+
+        if (block4Var != null) {
             block.add(jVarMT.invoke("getSwiftMessage").invoke("setBlock4").arg(block4Var));
         }
         block._return(jVarMT.invoke("message"));
